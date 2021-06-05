51c1c1d37a6396c1122fc5770326be240f626a16
16smerkel <16smerkel@gmail.com>
2021-06-05 12:10:17 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int firstNum = scanner.nextInt();
+        System.out.println("Enter the second number: ");
+        int secNum = scanner.nextInt();
+        System.out.println("Enter the third number: ");
+        int thirdNum = scanner.nextInt();
+        if(firstNum > secNum)
+        {
+            if(firstNum > thirdNum)
+            {
+                System.out.println("The largest number is " + firstNum + ".");
+            }
+            else if(thirdNum > firstNum)
+            {
+                System.out.println("The largest number is " + thirdNum + ".");
+            }
+        }
+        else if(secNum > firstNum)
+        {
+            if(secNum > thirdNum)
+            {
+                System.out.println("The largest number is " + secNum + ".");
+            }
+            else if(thirdNum > secNum)
+            {
+                System.out.println("The largest number is " + thirdNum + ".");
+            }
+        }
+        else if(thirdNum > firstNum)
+        {
+            if(thirdNum > secNum)
+            {
+                System.out.println("The largest number is " + thirdNum + ".");
+            }
+            else if(secNum > thirdNum)
+            {
+                System.out.println("The largest number is " + secNum + ".");
+            }
+        }
+    }
+}
+
